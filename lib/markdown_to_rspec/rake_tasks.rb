af31bcbe33554dd5397b2ce4c5ed061636822712
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-08-16 14:08:52 +0900

    :sparkles: raketasks separated the files.

---
+++ lib/markdown_to_rspec/rake_tasks.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rake'
+
+namespace :markdown_to_rspec do
+  desc 'converts file in markdown format into executable RSpec text.'
+  task :to_rspec_from_file, [:file_path] do |_task, args|
+    markdown_file_path = Pathname.new(args[:file_path].to_s)
+    if markdown_file_path.extname == '.md'
+      markdown_text = File.read(markdown_file_path)
+      puts MarkdownToRspec.to_rspec(markdown_text)
+    else
+      raise(MarkdownToRspec::InValidFileFormatError, 'Specify the file as a `.md` format.')
+    end
+  end
+
+  desc 'converts text in markdown format into executable RSpec text.'
+  task :to_rspec_from_text, [:text] do |_task, args|
+    # unescaped new line string.
+    markdown_text = args[:text].to_s.gsub('\\n', "\n")
+    puts MarkdownToRspec.to_rspec(markdown_text)
+  end
+
+  desc 'Show gem version.'
+  task :version do
+    puts "markdown_to_rspec: #{MarkdownToRspec::VERSION}"
+  end
+end
